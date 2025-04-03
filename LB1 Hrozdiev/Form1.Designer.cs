2dfa16ff34f229266a568a7ed4ec3ad5a9fdf874
Oleksandr Hrozdiev <154226251+mamoriSg@users.noreply.github.com>
2025-04-03 13:00:54 +0300

    01-Lb Hrozdiev

---
+++ LB1 Hrozdiev/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace LB_Prystupa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbArea = new System.Windows.Forms.TextBox();
+            this.tbPopulation = new System.Windows.Forms.TextBox();
+            this.tbAdminName = new System.Windows.Forms.TextBox();
+            this.tbAdminPop = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(142, 306);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(77, 32);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(347, 306);
+            this.btnShow.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(80, 32);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введіть назву області України:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введіть площу області України: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 133);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Введіть населення області України:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 189);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Введіть назву області України:";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(67, 41);
+            this.tbName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(213, 20);
+            this.tbName.TabIndex = 6;
+            // 
+            // tbArea
+            // 
+            this.tbArea.Location = new System.Drawing.Point(67, 96);
+            this.tbArea.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbArea.Name = "tbArea";
+            this.tbArea.Size = new System.Drawing.Size(213, 20);
+            this.tbArea.TabIndex = 7;
+            // 
+            // tbPopulation
+            // 
+            this.tbPopulation.Location = new System.Drawing.Point(69, 150);
+            this.tbPopulation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbPopulation.Name = "tbPopulation";
+            this.tbPopulation.Size = new System.Drawing.Size(211, 20);
+            this.tbPopulation.TabIndex = 8;
+            // 
+            // tbAdminName
+            // 
+            this.tbAdminName.Location = new System.Drawing.Point(67, 206);
+            this.tbAdminName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbAdminName.Name = "tbAdminName";
+            this.tbAdminName.Size = new System.Drawing.Size(213, 20);
+            this.tbAdminName.TabIndex = 9;
+            // 
+            // tbAdminPop
+            // 
+            this.tbAdminPop.Location = new System.Drawing.Point(67, 260);
+            this.tbAdminPop.Margin = new System.Windows.Forms.Padding(2);
+            this.tbAdminPop.Name = "tbAdminPop";
+            this.tbAdminPop.Size = new System.Drawing.Size(213, 20);
+            this.tbAdminPop.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(67, 243);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(238, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Введіть населення адміністративного центру:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.tbAdminPop);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbAdminName);
+            this.Controls.Add(this.tbPopulation);
+            this.Controls.Add(this.tbArea);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnSave);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "LB 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbArea;
+        private System.Windows.Forms.TextBox tbPopulation;
+        private System.Windows.Forms.TextBox tbAdminName;
+        private System.Windows.Forms.TextBox tbAdminPop;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ LB1 Hrozdiev/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LB_Prystupa
+{
+    public partial class Form1: Form
+    {
+        class Ukraine
+        {
+            
+           public string name;
+           public int area;
+           public int population;
+           public string nameadmin;
+           public int adminpop;
+
+           public Ukraine(string name, int area, int population, string nameadmin, int adminpop)
+            {
+
+                this.name = name;
+                this.area = area;
+                this.population = population;
+                this.nameadmin = nameadmin;
+                this.adminpop = adminpop;
+
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Ukraine fUkraine = null;
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            fUkraine = new Ukraine(tbName.Text, Convert.ToInt32(tbArea.Text), Convert.ToInt32(tbPopulation.Text), tbAdminName.Text, Convert.ToInt32(tbAdminPop.Text));
+
+            MessageBox.Show("Save success");
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Назва області України: " + fUkraine.name + "\n" +
+                "Площа області України: " + fUkraine.area + "\n" +
+                "Населення області України: " + fUkraine.population + "\n" +
+                "Назва адміністративного центру області України: " + fUkraine.nameadmin + "\n" +
+                "Населення адіміністративного центру: " + fUkraine.adminpop + "\n", "01-Lb_hrozdiev", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+
+        }
+    }
+}
